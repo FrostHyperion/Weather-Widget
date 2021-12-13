78828eaead5116f895b8a104dca0d22f463df606
sahil kumar <sahilnarwal1032@gmail.com>
2021-12-12 23:21:08 -0600

    getting data

--- script/app.js
+++ script/app.js
@@ -47,3 +47,10 @@ navigator.geolocation.getCurrentPosition((position) => {
     }
   );
 });
+const getData = async function (latitude, longitude) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
